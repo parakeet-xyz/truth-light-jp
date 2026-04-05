a3f6bc7b5a1e18aa80666dc2ef0a6e77c3bc6745
psy-yumekawa <psy.yumekawa@proton.me>
2026-04-05 17:30:12 +0900

    Fix: nuxt.config.ts content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
   app: {
     head: {
@@ -20,24 +18,17 @@ export default defineNuxtConfig({
     '~/assets/css/tailwind.css',
   ],
   site: {
-    url: 'https://www.truth-light.jp', // ←本番ドメイン
+    url: 'https://www.truth-light.jp',
     name: 'Truth Light',
   },
   sitemap: {
     sources: ['/api/__sitemap__/urls'],
   },
-  content: {
-    documentDriven: true,
-    contentHead: true,
-  },
   nitro: {
     routeRules: {
-      // ketcher配下だけ、iframe許可（同一オリジン前提）
       "/ketcher/**": {
         headers: {
-          // CSPでframe-ancestorsを self に（これが一番効くことが多い）
           "Content-Security-Policy": "frame-ancestors 'self';",
-          // もしどこかで DENY を付けてたら上書き（SAMEORIGINなら親が同一オリジンでOK）
           "X-Frame-Options": "SAMEORIGIN",
         },
       },
