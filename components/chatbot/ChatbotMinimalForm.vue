44b32eaeb374876ac897b3b78ba1dec11c52f50e
psy-yumekawa <psy.yumekawa@proton.me>
2026-05-02 15:34:38 +0900

    fix: loadingのimgのサイズ変更

--- components/chatbot/ChatbotMinimalForm.vue
+++ components/chatbot/ChatbotMinimalForm.vue
@@ -137,7 +137,7 @@ const submitPrompt = async (text: string): Promise<void> => {
       </div>
 
 
-      <div class="flex-1 px-4 py-4 ">
+      <div class="flex-1 px-4 py-4 h-10 w-10">
         <img src="/ui/loading-90.gif" />
       </div>
     </div>
