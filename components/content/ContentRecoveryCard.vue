b67021ba54fae2d70e790a8d7e3e3de88dd5705d
psy-yumekawa <psy.yumekawa@proton.me>
2026-05-16 01:49:56 +0900

    fix: RecoveryCard

--- components/content/ContentRecoveryCard.vue
+++ components/content/ContentRecoveryCard.vue
@@ -8,7 +8,7 @@ const substanceName = props.substanceName ?? '薬物'
 
 <template>
   <section class="flex flex-row">
-    <div><img src="yumekawa-ai/yumekawa-ai-01.png" alt="夢可愛AI アイコン" /></div>
+    <div><img src="/yumekawa-ai/yumekawa-ai-01.png" alt="夢可愛AI アイコン" /></div>
     <div>
       <p>{{ substanceName }}がやめれなくて困っていませんか？<br />Truth Lightでは薬物依存症からの回復の道を提供しています。</p>
       <p><NuxtLink :to="{name: 'docs/recovery'}" /></p>
