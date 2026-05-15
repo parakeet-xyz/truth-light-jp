f2603883de08102d127ef227f904c91b16220dc6
psy-yumekawa <psy.yumekawa@proton.me>
2026-05-16 01:11:00 +0900

    modify: Ecstasy記事

--- components/content/ContentRecoveryCard.vue
+++ components/content/ContentRecoveryCard.vue
@@ -14,4 +14,27 @@ const substanceName = props.substanceName ?? '薬物'
       <p><NuxtLink :to="{name: 'docs/recovery'}" /></p>
     </div>
   </section>
+
+  <div class="yumekawa-manga flex flex-row items-end">
+    <div class="flex flex-1 flex-row items-center justify-end">
+      <div class="flex flex-col w-[70%] min-h-16 rounded-lg bg-[#DDE4E7] px-3 py-2 items-center justify-center">
+        <p class="m-0">{{ substanceName }}がやめれなくて困っていませんか？<br />Truth Lightでは薬物依存症からの回復の道を提供しています。</p>
+        <p><NuxtLink :to="{name: 'docs/recovery'}" /></p>
+      </div>
+      <div class="flex-none">
+        <img
+          src="/yumekawa-manga/fukidashi-right-h128px.png"
+          alt="吹き出し"
+          class="h-16 w-auto"
+        />
+      </div>
+    </div>
+    <div class="flex-none">
+      <img
+        src="/yumekawa-manga/yumekawa-ai-02-128px.png"
+        alt="夢可愛アイ"
+        class="w-16 h-16"
+      />
+    </div>
+  </div>
 </template>
