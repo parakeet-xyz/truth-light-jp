0d5228eaee0903e2c1b3296ab0086a36b58f2ee5
psy-yumekawa <psy.yumekawa@proton.me>
2026-05-16 09:36:17 +0900

    fix: ContentRecoveryCard ファイルパスタイポ修正

--- components/content/ContentRecoveryCard.vue
+++ components/content/ContentRecoveryCard.vue
@@ -7,19 +7,11 @@ const substanceName = props.substanceName ?? '薬物'
 </script>
 
 <template>
-  <section class="flex flex-row">
-    <div><img src="/yumekawa-ai/yumekawa-ai-01.png" alt="夢可愛AI アイコン" /></div>
-    <div>
-      <p>{{ substanceName }}がやめれなくて困っていませんか？<br />Truth Lightでは薬物依存症からの回復の道を提供しています。</p>
-      <p><NuxtLink :to="{name: 'docs/recovery'}" /></p>
-    </div>
-  </section>
-
   <div class="yumekawa-manga flex flex-row items-end">
     <div class="flex flex-1 flex-row items-center justify-end">
       <div class="flex flex-col w-[70%] min-h-16 rounded-lg bg-[#DDE4E7] px-3 py-2 items-center justify-center">
-        <p class="m-0">{{ substanceName }}がやめれなくて困っていませんか？<br />Truth Lightでは薬物依存症からの回復の道を提供しています。</p>
-        <p><NuxtLink :to="{name: 'docs/recovery'}" /></p>
+        <p class="m-0">{{ substanceName }}が止めれなくて困っていませんか？</p>
+        <p>→→ {{ substanceName }}依存症からの回復方法について知る ←←</p>
       </div>
       <div class="flex-none">
         <img
